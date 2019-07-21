e2050783bfffc00dbc7f79603da8c3664a4a68a3
Henrique Vieira e Sousa <henviso@gmail.com>
2019-07-21 01:07:04 -0300

    Using sum as a submodule.

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "os"
+import "bufio"
+import "strings"
+import "strconv"
+import "./sum/sum"
+import "../mult/mult"
+
+func main() {
+	fmt.Println("Calculator")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Type your expression: ")
+	text, _ := reader.ReadString('\n')
+	s := strings.Split(text, " ")
+	sa, op, sb := s[0], s[1], s[2]
+	
+	a, _ := strconv.Atoi(sa)
+	b, _ := strconv.Atoi(sb)
+
+	switch op {
+		case "+": fmt.Printf("Sum %d + %d is %d", a, b, sum.Sum(a, b))
+		case "*": fmt.Printf("Mult %d * %d is %d", a, b, mult.Mult(a, b))
+		default: fmt.Println("Unsupported operation!")
+	}
+	fmt.Printf("\n")
+}
